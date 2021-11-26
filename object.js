27387ba6909503a29c8930142231aee804041006
ankitkrmehta <89727614+ankitkrmehta@users.noreply.github.com>
2021-11-26 20:36:11 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,14 @@
+class Person {
+  constructor(firstName, lastName, officeAdress) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.officeAdress = officeAdress;
+  }
+}
+var person1 = new Person("Prashant", "Kumar", "Khagaria");
+var person2 = new Person("Ankit", "Kumar", "Begusarai");
+var person3 = new Person("Pradeep", "Singh", "Bhabhua");
+
+console.log(person1);
+console.log(person2);
+console.log(person3);
